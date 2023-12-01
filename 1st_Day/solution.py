1c796e83780ae54e66883cb6c645f5d4d8a7e30b
Piotr Baran <piotrbaran0123@gmail.com>
2023-12-01 22:06:03 +0100

    1stDay - part two

--- 1st_Day/solution.py
+++ 1st_Day/solution.py
@@ -23,7 +23,8 @@ print(f"\033[33m|------------------------------------------|\033[0m")
 words_num = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', '1', '2', '3', '4', '5',
              '6', '7', '8', '9']
 
-num_dict = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
+num_dict = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9, '1': 1,
+            '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
 
 
 def calibrator_str(calibration_data):
@@ -33,11 +34,7 @@ def calibrator_str(calibration_data):
         n = pattern.findall(line)
         x = []
         for i in n:
-            if i.isdigit():
-                x.append(int(i))
-            else:
-                num = num_dict[i]
-                x.append(num)
+            x.append(num_dict[i])
         digit.append(int(str(x[0])+str(x[-1])))
     return digit
 
