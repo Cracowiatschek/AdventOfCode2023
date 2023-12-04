50103a85d44c1196de3b88a948fe6da6e5810b30
Piotr Baran <piotrbaran0123@gmail.com>
2023-12-04 19:22:56 +0100

    4thDay - part two - solved

--- 4th_Day/solution.py
+++ 4th_Day/solution.py
@@ -36,5 +36,50 @@ def score_card(filename):
 
 solution = score_card(file)
 print(f"\033[33m|-------------------------------------------------------|\033[0m")
-print(f"\033[33m|\033[0mTotal points from part one equal: \033[32m{solution}\033[33m|")
+print(f"\033[33m|\033[0mTotal points from part one equal: \033[32m{solution}\033[33m                |")
+print(f"\033[33m|-------------------------------------------------------|\033[0m")
+
+
+def how_many_cards(filename):
+    card_list = []
+    clear_card_list = []
+    for card in filename:
+        card_list.append(int(card.split(':')[0].split(' ')[-1]))
+        clear_card_list.append(int(card.split(':')[0].split(' ')[-1]))
+
+    id = 1
+
+    copy_paste = {}
+
+    for card in filename:
+        winning_numbers = []
+        my_numbers = []
+
+        for num in card.split('|')[0].split(' ')[2:]:
+            if num.isdigit():
+                winning_numbers.append(int(num))
+
+        for num in card.split('|')[1].split(' '):
+            if num.isdigit():
+                my_numbers.append(int(num))
+
+        match_numbers = list(set(my_numbers) & set(winning_numbers))
+
+        copy_paste[id] = len(match_numbers)
+        id += 1
+
+    id = 1
+    for i in clear_card_list:
+        to_copy = clear_card_list[id:id+copy_paste[id]]
+        cnt = card_list.count(i)
+        for n in range(cnt):
+            for copies in to_copy:
+                card_list.append(copies)
+
+        id += 1
+    return len(card_list)
+
+
+solution = how_many_cards(file)
+print(f"\033[33m|\033[0mTotal count of cards from part two equal: \033[32m{solution}\033[33m      |")
 print(f"\033[33m|-------------------------------------------------------|\033[0m")
