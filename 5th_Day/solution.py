f7c6487f6ab11669267cf1da3d22efb74679b0fa
Cracowiatschek <piotrbaran0123@gmail.com>
2023-12-06 23:21:00 +0100

    5thDay - complete |fck melt-brain|

--- 5th_Day/solution.py
+++ 5th_Day/solution.py
@@ -62,7 +62,9 @@ def gardener_map(filepath):
 
 solution = gardener_map(example)
 
-print(solution)
+print(f"\033[33m|------------------------------------------------------------|\033[0m")
+print(f"\033[33m|\033[0mThe lowest location number from part one equal: \033[32m{solution}\033[33m          |")
+print(f"\033[33m|------------------------------------------------------------|\033[0m")
 # part two | **
 
 
@@ -110,6 +112,34 @@ def gardener_extended_map(filepath):
 
     del garden_map['seeds']
 
+    ranges = seed_extended.copy()
 
-print(gardener_extended_map(example))
+    for maps in garden_map:
+
+        locate = []
+
+        while len(ranges) > 0:
+            min_rng, max_rng = ranges.pop()
+
+            for target, seed, rng in garden_map[maps]:
+                minimal_common_point = max(min_rng, seed)
+                maximum_common_point = min(max_rng, seed + rng)
+                delta = target - seed
+                if minimal_common_point < maximum_common_point:
+                    locate.append([minimal_common_point + delta, maximum_common_point + delta])
+                    if minimal_common_point > min_rng:
+                        ranges.append([min_rng, minimal_common_point])
+                    if max_rng > maximum_common_point:
+                        ranges.append([maximum_common_point, max_rng])
+                    break
+            else:
+                locate.append([min_rng, max_rng])
+        solve = min(locate)[0]
+        ranges = locate
+    return solve
+
+
+solution = gardener_extended_map(file)
+print(f"\033[33m|\033[0mThe lowest location number from part two equal: \033[32m{solution}\033[33m    |")
+print(f"\033[33m|------------------------------------------------------------|\033[0m")
 
